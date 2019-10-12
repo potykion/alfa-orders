b3e859664aa6bbcba1b5fa284a85363910aa25a1
potykion <potykion@gmail.com>
2019-10-12 22:18:01 +0300

    Alfa service usage

--- alfa_orders/api.py
+++ alfa_orders/api.py
@@ -9,6 +9,14 @@ from alfa_orders.session import create_alfa_session
 
 
 class AlfaService:
+    """
+    Usage:
+    >>> username, password = ("**********", "**********")
+    >>> service = AlfaService(username, password)
+    >>> from_date, to_date = dt.datetime(2019, 9, 1), dt.datetime(2019, 10, 1)
+    >>> transactions = list(service.get_transactions(from_date, to_date))
+    >>> refunds = list(service.get_refunds(from_date, to_date))
+    """
     def __init__(self, username: str, password: str, config: AlfaConfig = None) -> None:
         self.config = config or AlfaConfig()
         self.session = create_alfa_session(username, password, self.config)
